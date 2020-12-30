2e63b2d36224a33868ea99461750f3175cf1c62d
Rafael Garcia <rafaelgarcia094@gmail.com>
2020-12-30 12:31:54 +0100

    Refactoring code, removing matplotlib functionality, making the code compliant with PEP 8 formatting

--- src/fmu/sumo/_upload_files.py
+++ src/fmu/sumo/_upload_files.py
@@ -1,54 +1,51 @@
-import logging
+from concurrent.futures import ThreadPoolExecutor
 
-def UPLOAD_FILES(files: list, sumo_parent_id: str, sumo_connection, threads=4):
-    """
-    Upload files, including JSON/manifest, to specified ensemble
 
-    files: list of FileOnDisk objects
-    sumo_parent_id: sumo_parent_id for the parent ensemble
+def _upload_files(files, sumo_connection, sumo_parent_id, threads=4):
+    with ThreadPoolExecutor(threads) as executor:
+        results = executor.map(_upload_file, [(file, sumo_connection, sumo_parent_id) for file in files])
 
-    Upload is kept outside classes to use multithreading.
-    """
+    return results
+
+
+def _upload_file(args):
+    file, sumo_connection, sumo_parent_id = args
+
+    result = file.upload_to_sumo(sumo_connection=sumo_connection, sumo_parent_id=sumo_parent_id)
     
-    import time
-    from datetime import datetime
-    from concurrent.futures import ThreadPoolExecutor
+    return result
 
-    def _upload_files(files, sumo_connection, sumo_parent_id, threads=threads):
-        with ThreadPoolExecutor(threads) as executor:
-            results = executor.map(_upload_file, [(file, sumo_connection, sumo_parent_id) for file in files])
-        return results
 
-    def _upload_file(arg):
-        file, sumo_connection, sumo_parent_id = arg
-        if not file:
-            return
-        result = file.upload_to_sumo(sumo_connection=sumo_connection, sumo_parent_id=sumo_parent_id)
-        return result
+def upload_files(files: list, sumo_parent_id: str, sumo_connection, threads=4):
+    """
+        Upload files, including JSON/manifest, to specified ensemble
 
-    results = _upload_files(files=files, sumo_connection=sumo_connection, sumo_parent_id=sumo_parent_id, threads=threads)
+        files: list of FileOnDisk objects
+        sumo_parent_id: sumo_parent_id for the parent ensemble
+
+        Upload is kept outside classes to use multithreading.
+    """
+
+    results = _upload_files(files=files, sumo_connection=sumo_connection,
+                            sumo_parent_id=sumo_parent_id, threads=threads)
 
     ok_uploads = []
     failed_uploads = []
     rejected_uploads = []
 
     for r in results:
-        _status = r.get('status')
-        if not _status:
+        status = r.get('status')
+        
+        if not status:
             raise ValueError('File upload result returned with no "status" attribute')
-        if _status == 'ok':
+        
+        if status == 'ok':
             ok_uploads.append(r)
-        elif _status == 'rejected':
+
+        elif status == 'rejected':
             rejected_uploads.append(r)
+
         else:
             failed_uploads.append(r)
 
-    if not (len(ok_uploads)+len(failed_uploads)+len(rejected_uploads) == len(files)):
-        raise ValueError(f'Sum of ok_uploads ({len(ok_uploads)}, rejected_uploads ({len(rejected_uploads)}) ' \
-                         f'and failed_uploads ({len(failed_uploads)}) does not add up to ' \
-                         f'number of files total ({len(files)})' \
-                         )
-
     return {'ok_uploads': ok_uploads, 'failed_uploads': failed_uploads, 'rejected_uploads': rejected_uploads}
-
-
