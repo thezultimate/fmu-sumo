4212036b6fbddf55b21cdafe082b8802f1f71776
Per Olav Eide Svendsen <peesv@statoil.com>
2021-04-07 19:08:23 +0200

    Updated tests, updated testdata

--- tests/test_uploader.py
+++ tests/test_uploader.py
@@ -10,17 +10,30 @@ from fmu.sumo import uploader
 
 
 def test_uploader():
-    raise NotImplementedError('Must be refactored for Drogon')
+    #raise NotImplementedError('Must be refactored for Drogon')
 
-    manifest_path = 'data/MyCaseName2/fmu_ensemble.yaml'
-    search_path = 'data/MyCaseName2/'
+    ensemble_metadata_path = 'data/test_ensemble_070/ensemble.yaml'
+    search_path = 'data/test_ensemble_070/'
     env = 'dev'
     threads = 1
 
     sumo_connection = uploader.SumoConnection(env=env)
-    e = uploader.EnsembleOnDisk(manifest_path=manifest_path, sumo_connection=sumo_connection)
+    e = uploader.EnsembleOnDisk(ensemble_metadata_path=ensemble_metadata_path, sumo_connection=sumo_connection)
     with pytest.warns(UserWarning) as warnings_record:  # testdata contains one file with missing metadata
         e.add_files(search_path + '/*.bin')
+
+    with pytest.raises(IOError):
+        # assert that uploading withouth registering fails
+        e.upload(threads=threads)
+
+    e.register()
+
+    # Assert Ensemble is on Sumo
+    query = f'fmu.ensemble.id:{e.fmu_ensemble_id}'
+    search_results = sumo_connection.api.searchroot(query, select="source", buckets="source")
+    hits = search_results.get('hits').get('hits')
+    assert len(hits) == 1
+
     e.upload(threads=threads)
 
     # Assert that expected warning was given
@@ -28,12 +41,6 @@ def test_uploader():
         print(w.message.args[0])
     assert len(warnings_record) == 1
     assert warnings_record[0].message.args[0].endswith("No metadata, skipping file.")
-
-    # Assert Ensemble is on Sumo
-    query = f'fmu_ensemble_id:{e.fmu_ensemble_id}'
-    search_results = sumo_connection.api.searchroot(query, select="source", buckets="source")
-    hits = search_results.get('hits').get('hits')
-    assert len(hits) == 1
 
     # Assert children is on Sumo
     search_results = sumo_connection.api.search(query=f'{e.fmu_ensemble_id}')
