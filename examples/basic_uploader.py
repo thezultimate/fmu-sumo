06e2eb59001d5a9717ea27dd6bd610176283465b
Per Olav Eide Svendsen <peesv@statoil.com>
2021-04-09 19:21:39 +0200

    refactor basic_uploader.py

--- examples/basic_uploader.py
+++ examples/basic_uploader.py
@@ -6,22 +6,17 @@ import os
 import sys
 import argparse
 
-try:
-    sys.path.index('../src')
-except ValueError:
-    sys.path.append('../src')
-
 from fmu.sumo import uploader
 
-
 def main():
-    manifest_path, search_path, env, threads = parse_arguments()
-
-    sumo_connection = uploader.SumoConnection(env=env)
-    e = uploader.EnsembleOnDisk(manifest_path=manifest_path, sumo_connection=sumo_connection)
-    e.add_files(search_path + '/*.bin')
-    e.upload(threads=threads, register_ensemble=True)
-
+    args = parse_arguments()
+    sumo_connection = uploader.SumoConnection(env=args.env)
+    e = uploader.EnsembleOnDisk(
+        ensemble_metadata_path=args.ensemble_metadata_path,
+        sumo_connection=args.sumo_connection
+        )
+    e.add_files(args.search_path)
+    e.upload(threads=args.threads, register_ensemble=True)
 
 def parse_arguments():
     """
@@ -33,18 +28,16 @@ def parse_arguments():
 
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('manifest_path', type=str, help='Absolute path to run manifest (ensemble)')
-    parser.add_argument('search_path', type=str, help='Global search path for files to upload')
-    parser.add_argument('env', type=str, help="Which environment to use.")
-    parser.add_argument('threads', type=int, help="Set number of threads to use.")
+    parser.add_argument('ensemble_metadata_path', type=str, help='Absolute path to ensemble metadata')
+    parser.add_argument('search_path', type=str, help='Search path for files to upload')
+    parser.add_argument('env', type=str, default='prod', help="Sumo environment. Default: prod")
+    parser.add_argument('threads', type=int, default=8, help="Number of threads to use. Default: 8")
     args = parser.parse_args()
 
-    if args.env not in ['dev', 'test', 'prod', 'exp']:
-        raise ValueError(f'Illegal environment: {args.env}. Valid environments: dev, test, prod, exp')
+    if args.env not in ['dev', 'test', 'prod', 'exp', 'fmu']:
+        raise ValueError(f'Illegal environment: {args.env}. Valid environments: dev, test, prod, exp, fmu')
 
-    manifest_path = os.path.join(args.manifest_path, 'fmu_ensemble.yaml')
-
-    return manifest_path, args.search_path, args.env, args.threads
+    return args
 
 
 if __name__ == '__main__':
