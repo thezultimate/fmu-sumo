b16f157d2bae6d7ab898874c599824842a3a5c02
Per Olav Eide Svendsen <peesv@statoil.com>
2021-04-29 20:57:19 +0200

    black

--- examples/basic_uploader.py
+++ examples/basic_uploader.py
@@ -14,37 +14,47 @@ import argparse
 
 from fmu.sumo import uploader
 
+
 def main():
     args = parse_arguments()
     sumo_connection = uploader.SumoConnection(env=args.env)
     e = uploader.EnsembleOnDisk(
         ensemble_metadata_path=args.ensemble_metadata_path,
-        sumo_connection=sumo_connection
-        )
+        sumo_connection=sumo_connection,
+    )
     e.add_files(args.search_path)
     e.upload(threads=args.threads, register_ensemble=True)
+
 
 def parse_arguments():
     """
 
-        Parse the arguments
+    Parse the arguments
 
-        Returns:
-            args: argparse.ArgumentParser() object
+    Returns:
+        args: argparse.ArgumentParser() object
 
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('ensemble_metadata_path', type=str, help='Absolute path to ensemble metadata')
-    parser.add_argument('search_path', type=str, help='Search path for files to upload')
-    parser.add_argument('env', type=str, default='prod', help="Sumo environment. Default: prod")
-    parser.add_argument('threads', type=int, default=8, help="Number of threads to use. Default: 8")
+    parser.add_argument(
+        "ensemble_metadata_path", type=str, help="Absolute path to ensemble metadata"
+    )
+    parser.add_argument("search_path", type=str, help="Search path for files to upload")
+    parser.add_argument(
+        "env", type=str, default="prod", help="Sumo environment. Default: prod"
+    )
+    parser.add_argument(
+        "threads", type=int, default=8, help="Number of threads to use. Default: 8"
+    )
     args = parser.parse_args()
 
-    if args.env not in ['dev', 'test', 'prod', 'exp', 'fmu', 'preview']:
-        raise ValueError(f'Illegal environment: {args.env}. Valid environments: dev, test, prod, exp, fmu, preview')
+    if args.env not in ["dev", "test", "prod", "exp", "fmu", "preview"]:
+        raise ValueError(
+            f"Illegal environment: {args.env}. Valid environments: dev, test, prod, exp, fmu, preview"
+        )
 
     return args
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
