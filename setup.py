b68accaf23bd897de0ad1e0b54d2506d71606add
Øistein Haaland <oihaa@statoil.com>
2021-09-07 13:14:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="fmu-sumo",
     description="Python package for interacting with Sumo in an FMU setting",
     url="https://github.com/equinor/fmu-sumo",
-    version="0.1",
+    version="0.1.0",
     author="Per Olav Svendsen",
     license="GPLv3",
     keywords="fmu, sumo",
