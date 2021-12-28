2c7a1e93235f6c8a1c428da6520b38cca4da0e8d
Per Olav Eide Svendsen <peesv@equinor.com>
2021-12-28 12:02:17 +0100

    Change author to Equinor and add Python 3.6

--- setup.py
+++ setup.py
@@ -8,31 +8,30 @@ setup(
     description="Python package for interacting with Sumo in an FMU setting",
     url="https://github.com/equinor/fmu-sumo",
     version="0.1.2",
-    author="Per Olav Svendsen",
+    author="Equinor",
     license="GPLv3",
     keywords="fmu, sumo",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
     author_email="peesv@equinor.com",
     entry_points={
-        "ert": [
-            "fmu_sumo_jobs = jobs.hook_implementations.jobs"
-        ],
+        "ert": ["fmu_sumo_jobs = jobs.hook_implementations.jobs"],
         "console_scripts": [
             "sumo_upload=fmu.sumo.uploader.scripts.fm_fmu_uploader:main"
-        ]
+        ],
     },
     install_requires=[
         "PyYAML",
         "pandas",
         "sumo-wrapper-python",
         "setuptools",
-        "oneseismic"
+        "oneseismic",
     ],
     python_requires=">=3.4",
     packages=find_packages("src"),
